ce6d1a9d448bd871c4bfb17f7753944eeaf10239
bbottema <b.bottema@projectnibble.org>
2015-09-24 12:54:34 +0000

    dynamic mimetype based on image url

---
+++ src/js/util/handlebar-helpers.js
@@ -0,0 +1,25 @@
+/* global Handlebars */
+(function() {
+    'use strict';
+    
+    var mimetypes = {
+        "jpeg": "image/jpeg",
+        "jpg": "image/jpeg",
+        "bmp": "image/bmp",
+        "png": "image/png",
+        "svg": "image/svg+xml",
+        "gif": "image/gif"
+    };
+
+    Handlebars.registerHelper('extension', function(str) {
+        return ext(str);
+    });
+    
+    Handlebars.registerHelper('mimetype', function(str) {
+        return mimetypes[ext(str)];
+    });
+    
+    function ext(str) {
+        return str.substr(str.lastIndexOf('.') + 1);
+    }
+})();
