1a054e7aa89bc6b0936a826c5ef8892e23180fab
bbottema <b.bottema@projectnibble.org>
2015-09-25 12:23:13 +0000

    code style issues

--- src/js/template-builders/idpf-wasteland-builder.js
+++ src/js/template-builders/idpf-wasteland-builder.js
@@ -95,15 +95,15 @@
             function formatHTML(htmlstr) {
                 return (skipFormatting || typeof html_beautify === 'undefined') ? htmlstr : 
                     html_beautify(htmlstr, {
-                        end_with_newline: false,
-                        indent_char: '\t',
-                        indent_inner_html: true,
-                        indent_size: '1',
-                        preserve_newlines: false,
-                        wrap_line_length: '0',
-                        unformatted: [],
-                        selector_separator_newline: false,
-                        newline_between_rules: true
+                        'end_with_newline': false,
+                        'indent_char': '\t',
+                        'indent_inner_html': true,
+                        'indent_size': '1',
+                        'preserve_newlines': false,
+                        'wrap_line_length': '0',
+                        'unformatted': [],
+                        'selector_separator_newline': false,
+                        'newline_between_rules': true
                     });
             }
         }
