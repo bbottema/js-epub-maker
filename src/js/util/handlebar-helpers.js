30087d7cf41050705379ecabe218c592dc4e2d98
bbottema <b.bottema@projectnibble.org>
2015-09-25 12:04:03 +0000

    quote consistency

--- src/js/util/handlebar-helpers.js
+++ src/js/util/handlebar-helpers.js
@@ -3,12 +3,12 @@
     'use strict';
     
     var mimetypes = {
-        "jpeg": "image/jpeg",
-        "jpg": "image/jpeg",
-        "bmp": "image/bmp",
-        "png": "image/png",
-        "svg": "image/svg+xml",
-        "gif": "image/gif"
+        'jpeg': 'image/jpeg',
+        'jpg': 'image/jpeg',
+        'bmp': 'image/bmp',
+        'png': 'image/png',
+        'svg': 'image/svg+xml',
+        'gif': 'image/gif'
     };
 
     Handlebars.registerHelper('extension', function(str) {
